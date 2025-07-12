e7390f1673c09716f245903f4a681b9e2bc47824
Angel23405 <afeezhifz@gmail.com>
2025-07-12 17:44:17 +0100

    AGRREGATE FUNCTIONS

---
+++ ola.sql
@@ -0,0 +1,23 @@
+CREATE TABLE friends(
+    user_id INTEGER PRIMARY KEY,
+    name TEXT,
+    fav_color TEXT,
+    monthly_salary REAL
+);
+
+-- Inserting data into Salesman
+INSERT INTO friends VALUES
+(5001, 'James Hoog', 'purple',100000),
+
+(5002, 'Nail Knite', 'yellow', 50000),
+(5005, 'Pit Alex', 'orange', 200000),
+(5006, 'Mc Lyon', 'purple', 70000),
+(5007, 'Paul Adam', 'yellow', 4000000),
+(5003, 'Lauson Hen', 'pink', 40000);
+
+SELECT * FROM friends;
+
+SELECT COUNT(NAME) FROM friends;
+SELECT AVG(MONTHLY_SALARY) FROM friends;
+SELECT SUM(MONTHLY_SALARY) FROM friends;
+select DISTINCT(FAV_COLOR) FROM friends;
