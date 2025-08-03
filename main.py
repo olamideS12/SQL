389350c530ff0797f7d554e5b4672a6b85fc29ff
Angel23405 <afeezhifz@gmail.com>
2025-08-03 17:53:04 +0100

    count,distint,min,max,avg

--- main.py
+++ main.py
@@ -10,19 +10,19 @@ import pandas as pd
 tables = pd.read_sql(""" 
 select * from sqlite_master WHERE type = 'table';
 """, conn)
-#print(tables)
+print(tables)
 
 Match = pd.read_sql("""
 select * from Match;
 """, conn)
 
-print(Match)
+#print(Match)
 
 Max_min_margin = pd.read_sql("""
 select Max(Win_Margin),Min(Win_Margin)
 from Match;
 """, conn)
-print (Max_min_margin)
+#print (Max_min_margin)                                                                   
 
 Match_winner7 = pd.read_sql("""
 select * from Match 
@@ -35,13 +35,32 @@ select * from City;
 """, conn)
 #print(City)
 
-Team = pd.read_sql("""
-select * from Team;
+Season = pd.read_sql("""
+select * from Season;
 """, conn)
-#print(Team)
+print(Season)
 
 New_team = pd.read_sql(""" 
 select * from Team
 where Team_name like '%D%';
 """, conn)
 #print(New_team)
+
+Venue = pd.read_sql(""" 
+select * from Venue;
+                    
+""",conn)
+#print(Venue)
+
+
+Eden_gardens = pd.read_sql("""
+select count (distinct(venue_id)) from match
+where Season_Id =5;
+""",conn)
+print(Eden_gardens)
+
+Result = pd.read_sql("""
+Select max(Win_Margin),min(Win_Margin),AVG(Win_Margin)
+From Match; 
+""",conn)
+print(Result)
